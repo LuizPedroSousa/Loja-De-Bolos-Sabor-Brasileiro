5bf57911ef84d30f29525a6b3438c386cb64c1c0
LuizPedroSousa <luizpedrosousa64@gmail.com>
2021-06-19 19:20:26 -0300

    fix(usePersistedState): fix ssr :bug:

--- src/hooks/usePersistedState.ts
+++ src/hooks/usePersistedState.ts
@@ -1,3 +1,4 @@
+import Cookies from 'js-cookie'
 import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
 type Response<S> = [S, Dispatch<SetStateAction<S>>]
@@ -6,20 +7,16 @@ export default function usePersistedState<S>(
     initialState: S,
     key: string
 ): Response<S> {
-    const [state, setState] = useState<any>([])
+    const [state, setState] = useState(() => {
+        const storageValue = Cookies.get(key)
+        if (storageValue) {
+            return JSON.parse(storageValue)
+        }
+        return initialState
+    })
 
     useEffect(() => {
-        setState(() => {
-            const storageValue = localStorage.getItem(key)
-            if (storageValue) {
-                return JSON.parse(storageValue)
-            }
-            return initialState
-        })
-    }, [])
-
-    useEffect(() => {
-        localStorage.setItem(key, JSON.stringify(state))
+        Cookies.set(key, JSON.stringify(state))
     }, [state, key])
 
     return [state, setState]
